05e30d417abb35b3efa84212bd8f6cec1c532903
Zakhar <ainimwo@gmail.com>
2022-08-30 23:46:23 +0200

    домашка 4 адача 8, на выходе показывает все чётные числа от 1 до N

---
+++ HW004/HW008.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.*/
+
+
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("------------");
+int i = 1;
+//int i = num* (-1);
+if (num < 0)
+{
+    i = -1;
+    while (i > num)
+    {
+        Console.WriteLine(i);
+        i--;
+    }
+}
+
+while (i <= num)
+{
+    Console.WriteLine(i);
+    i++;
+}
