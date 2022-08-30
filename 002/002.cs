9e0613769d28c13bb07604c2f8674682814099d6
Zakhar <ainimwo@gmail.com>
2022-08-30 22:10:48 +0200

    вторая задача являетсяли первое число квадратом второго

---
+++ 002/002.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая на вход принимает два числа и проверяет, являетсяли первое число квадратом второго.
+a = 25, b = 5 -> да 
+a = 2, b = 10 -> нет 
+a = 9, b = -3 -> да 
+a = -3 b = 9 -> нет
+*/
+
+double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+
+if ( a / b == b){
+    Console.WriteLine("Число " + a + "является квадратом числа" + b);
+}
+else{
+   Console.WriteLine("Число " + a + " не является квадратом числа" + b); 
+}
