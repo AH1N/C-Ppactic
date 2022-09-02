b6e07543f72d1701cedbf2d13753f3a7fb210351
Zakhar <ainimwo@gmail.com>
2022-09-02 21:58:34 +0200

    добавил задачу 13

---
+++ HW010/hw010.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int num1 = new Random ().Next(100,1000);
+Console.WriteLine(num1);
+int secondDigit = (num1 / 10) % 10;
+Console.WriteLine(secondDigit);
+Console.WriteLine("=============================================");
+
+int num2 = Convert.ToInt32(Console.ReadLine());
+int twoSecondDigit = (num2 / 10) % 10;
+Console.WriteLine(twoSecondDigit);
+Console.WriteLine("=============================================");
+
+string num3 = Console.ReadLine();
+Console.WriteLine(num3[1]);
---
+++ HW013/hw013.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+
+32679 -> 6*/ 
+
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num2 / 100 == 0) {
+    Console.WriteLine("ВСЁ ПРОПАЛО, НЕТ ТРЕТЬЕЙ ЦИФРЫ");
+}
+else{
+    Console.WriteLine(num2 % 10);
+}
+Console.WriteLine("=============================================");
+
+
+string num3 = Console.ReadLine();
+if(num3.Length < 3) Console.WriteLine("ВСЁ ПРОПАЛО, НЕТ ТРЕТЬЕЙ ЦИФРЫ");
+else Console.WriteLine(num3[2]);
+
