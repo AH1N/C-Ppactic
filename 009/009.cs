bc2c6c749aa2f8c5f43d17a13fd1abbcae72b375
Zakhar <ainimwo@gmail.com>
2022-09-02 19:34:54 +0200

    009

---
+++ 009/009.cs
@@ -0,0 +1,8 @@
+﻿//случайное число из отрезка 10,99 и показывает наибольшее
+int num = Convert.ToInt32(Console.Readline());
+
+int FirstNum = num / 10;
+int SecondNum = num % 10;
+
+int max = FirstNum;
+if(SecondNum > max) SecondNum = max;
