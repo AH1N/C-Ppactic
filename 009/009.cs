6cf6c6f22182657b3d17f10534fdf910dee5eeb9
Zakhar <ainimwo@gmail.com>
2022-09-02 20:16:14 +0200

    добавил практику на уроке

--- 009/009.cs
+++ 009/009.cs
@@ -1,8 +1,10 @@
 ﻿//случайное число из отрезка 10,99 и показывает наибольшее
-int num = Convert.ToInt32(Console.Readline());
+int num = Convert.ToInt32(Console.ReadLine());
 
 int FirstNum = num / 10;
 int SecondNum = num % 10;
 
 int max = FirstNum;
-if(SecondNum > max) SecondNum = max;
+if(SecondNum > max) max = SecondNum;
+
+Console.WriteLine($"{max} это число максимальное");
---
+++ 011/Program.cs
@@ -0,0 +1,16 @@
+﻿/*11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+int num = new Random ().Next(100,999);
+Console.WriteLine(num);
+
+int num2 = num / 100;
+
+int num1 = num % 10;
+
+Console.Write(num2);
+Console.Write(num1);
+
+
