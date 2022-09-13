bed457315bfd5ca99fa8957709776017b1010d24
Zakhar <ainimwo@gmail.com>
2022-09-13 20:56:41 +0200

    Добавлена практика от 13.09.2022

---
+++ 031/031.cs
@@ -0,0 +1,42 @@
+﻿/*Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+Найдите сумму отрицательных и положительных элементов массива.
+Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных 
+чисел равна 29, сумма отрицательных равна -20.
+*/
+
+int size = Convert.ToInt32(Console.ReadLine());
+//int num  = Convert.ToInt32(Console.ReadLine());
+
+int[] arr= new int[size];
+for (int i = 0; i < size; i++)
+{
+    arr[i] = new Random().Next(-9,10);
+    
+}
+
+System.Console.WriteLine(String.Join(" ,", arr));
+int countP = 0;
+int countN = 0;
+
+for (int i = 0; i < size; i++)
+{
+    if (arr[i] > 0) countP += arr[i];
+    else countN += arr[i];
+}
+
+
+Console.WriteLine(countP);
+System.Console.WriteLine(countN);
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 33/033.cs
@@ -0,0 +1,94 @@
+﻿/*Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+4; массив [6, 7, 19, 345, 3] -> нет
+-3; массив [6, 7, 19, 345, 3] -> да
+
+Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5
+*/
+
+
+
+// int size =Convert.ToInt32(Console.ReadLine());
+
+
+// int[] arr= new int[size];
+// for (int i = 0; i < size; i++)
+// {
+//     arr[i] = new Random().Next(-9,10);
+
+// }
+
+// System.Console.WriteLine(String.Join(" ,", arr));
+// for(int i = 0; i < size; i++){
+//     arr[i] *= (-1);
+// }
+
+// System.Console.WriteLine(String.Join(" ,", arr));
+
+/*Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+4; массив [6, 7, 19, 345, 3] -> нет
+-3; массив [6, 7, 19, 345, 3] -> да*/
+
+
+// int size = Convert.ToInt32(Console.ReadLine());
+// int num  = Convert.ToInt32(Console.ReadLine());
+
+// int[] arr= new int[size];
+// for (int i = 0; i < size; i++)
+// {
+//     arr[i] = new Random().Next(-9,10);
+    
+// }
+
+// System.Console.WriteLine(String.Join(" ,", arr));
+
+// int count = 0;
+// for (int i = 0; i < size; i++)
+// {
+//     if(arr[i] == num) 
+//     {
+//         count = 1 ;
+//         break;
+//     }
+//     else count = 0;
+// }
+
+// if(count == 1) System.Console.WriteLine(num + "присутствует в массиве" + " " + (String.Join(" ,", arr)));
+// else System.Console.WriteLine(num + " НЕ присутствует в массиве" + (String.Join(" ,", arr)));
+
+
+/*Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5*/
+
+
+// int size = Convert.ToInt32(Console.ReadLine());
+// int num  = Convert.ToInt32(Console.ReadLine());
+
+// int[] arr= new int[size];
+// for (int i = 0; i < size; i++)
+// {
+//     arr[i] = new Random().Next(1,122);
+    
+// }
+
+// System.Console.WriteLine(String.Join(" ,", arr));
+// int count = 0;
+
+// for (int i = 0; i < size; i++)
+// {
+//     if (arr[i] >= 10 && arr[i] <= 99)
+//     {
+//         count++;
+//     }
+// }
+
+// Console.WriteLine(count);
